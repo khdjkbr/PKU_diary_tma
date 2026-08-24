d250ba14c50d0676e549689e2097bff1c4312d4c
khdjkbr <140163401+khdjkbr@users.noreply.github.com>
2026-08-25 00:25:11 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js — Ключи доступа к базе данных Supabase
+const SUPABASE_URL = "https://lkvoxthapdybhyyqghfl.supabase.co";
+const SUPABASE_KEY = "ВeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxrdm94dGhhcGR5Ymh5eXFnaGZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1OTQwOTUsImV4cCI6MjEwMzE3MDA5NX0.-Sh-ir0g27jpyDzC4yTsemOw2eoVue6sRWuJo0Hwttc";
